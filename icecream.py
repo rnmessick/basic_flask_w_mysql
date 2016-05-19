7d274d0664fdb2c69dd0d42026f180a9f7f67c84
meadch <meadch@gmail.com>
2016-05-19 16:51:47 -0700

    links database response to front-end

--- icecream.py
+++ icecream.py
@@ -6,9 +6,13 @@ app = Flask(__name__)
 # Create DB connection
 mysql = MySQLConnector(app, 'ice_cream_db')
 
+
+
 @app.route('/', methods=["GET"])
 def ice_cream_index():
-    return render_template('icecream.html')
+    ice_creams = mysql.query_db("SELECT name FROM ice_creams")
+
+    return render_template('icecream.html', icecreams = ice_creams)
 
 if __name__ == "__main__":
     app.run(debug=True)
