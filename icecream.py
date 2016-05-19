453383ed7136b29c717f12181baf1009f170b3c6
meadch <meadch@gmail.com>
2016-05-19 16:48:46 -0700

    establishes database connection

--- icecream.py
+++ icecream.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
+from DBConnection import MySQLConnector
 
 app = Flask(__name__)
+
+# Create DB connection
+mysql = MySQLConnector(app, 'ice_cream_db')
 
 @app.route('/', methods=["GET"])
 def ice_cream_index():
