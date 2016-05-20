a4f8a2f1863745afe85ef73ebecea1e8f1c34a3b
meadch <meadch@gmail.com>
2016-05-19 17:02:31 -0700

    adds create functionality

--- icecream.py
+++ icecream.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from DBConnection import MySQLConnector
 
 app = Flask(__name__)
@@ -10,9 +10,20 @@ mysql = MySQLConnector(app, 'ice_cream_db')
 
 @app.route('/', methods=["GET"])
 def ice_cream_index():
-    ice_creams = mysql.query_db("SELECT name FROM ice_creams")
-
+    query = "SELECT name FROM ice_creams"
+    data = {}
+    ice_creams = mysql.query_db(query, data)
     return render_template('icecream.html', icecreams = ice_creams)
+
+@app.route('/icecreams', methods=["POST"])
+def create_ice_creams():
+    query = "INSERT INTO ice_creams (name, created_at, updated_at) VALUES ( :name, NOW(), NOW())"
+    data = {
+        "name" : request.form['name']
+    }
+    mysql.query_db(query, data)
+
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run(debug=True)
